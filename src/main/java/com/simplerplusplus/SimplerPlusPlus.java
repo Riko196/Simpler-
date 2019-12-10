fc5129bc6f94c9ab1d74da46c9fc86f16c38355f
Erik Kučák <riko98@azet.sk>
2019-12-10 14:36:59 +0100

    Created main function and input.txt

--- src/main/java/com/simplerplusplus/SimplerPlusPlus.java
+++ src/main/java/com/simplerplusplus/SimplerPlusPlus.java
@@ -1,7 +1,25 @@
 package com.simplerplusplus;
 
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import com.visitor.SimplerPlusPlusLexer;
+import com.visitor.SimplerPlusPlusParser;
+import com.visitor.SimplerPlusPlusVisitor;
+
+import java.io.IOException;
+
+
 public class SimplerPlusPlus {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        CharStream in = CharStreams.fromFileName("input.txt");
+        SimplerPlusPlusLexer lexer = new SimplerPlusPlusLexer(in);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SimplerPlusPlusParser parser = new SimplerPlusPlusParser(tokens);
+
+        ParseTree tree = parser.root();
+
+        SimplerPlusPlusVisitor visitor = new SimplerPlusPlusVisitor();
+        visitor.visit(tree);
     }
 }
