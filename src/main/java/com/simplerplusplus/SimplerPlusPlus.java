7f50fbffce474fc2bcb7ded29d2d9b4188befe98
Erik Kučák <riko98@azet.sk>
2020-02-08 03:58:56 +0100

    Accomplished main function with files

--- src/main/java/com/simplerplusplus/SimplerPlusPlus.java
+++ src/main/java/com/simplerplusplus/SimplerPlusPlus.java
@@ -7,12 +7,14 @@ import com.visitor.SimplerPlusPlusLexer;
 import com.visitor.SimplerPlusPlusParser;
 import com.visitor.SimplerPlusPlusVisitor;
 
-import java.io.IOException;
+import com.code_fragment.*;
 
+import java.io.*;
 
 public class SimplerPlusPlus {
     public static void main(String[] args) throws IOException {
-        CharStream in = CharStreams.fromFileName("input.txt");
+        CharStream in = CharStreams.fromFileName("./src/main/java/com/simplerplusplus/input.txt");
+
         SimplerPlusPlusLexer lexer = new SimplerPlusPlusLexer(in);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         SimplerPlusPlusParser parser = new SimplerPlusPlusParser(tokens);
@@ -20,6 +22,12 @@ public class SimplerPlusPlus {
         ParseTree tree = parser.root();
 
         SimplerPlusPlusVisitor visitor = new SimplerPlusPlusVisitor();
-        visitor.visit(tree);
+
+        CodeFragment root = visitor.visit(tree);
+
+        System.out.println(root);
+        FileWriter llvmOutput = new FileWriter("./src/main/java/com/simplerplusplus/llvm_output.ll");
+        llvmOutput.write(root.toString());
+        llvmOutput.close();
     }
 }
